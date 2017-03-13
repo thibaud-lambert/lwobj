5804c75ed8d5022b089004203138b560b87de5f8
Thibaud Lambert <thibaud.lambert@gmail.com>
2017-03-13 19:50:06 +0100

    Adds write method

--- src/obj.rs
+++ src/obj.rs
@@ -1,4 +1,5 @@
 use std::io::BufRead;
+use std::io::Write;
 use std::io;
 use std::str::SplitWhitespace;
 use std::str::FromStr;
@@ -67,7 +68,6 @@ impl ObjData {
         }
     }
 
-
     /// Load an `ObjData` from a `BufReader`.
     ///
     /// # Examples
@@ -135,12 +135,12 @@ impl ObjData {
                                 Err(_) => return Err(LoadingError::Parse(nb)),
                             };
                             let vt = match index[1].parse::<usize>().ok() {
-                                    Some(val) => Some(val-1),
-                                    None => None,
+                                Some(val) => Some(val-1),
+                                None => None,
                             };
                             let vn = match index[2].parse::<usize>().ok() {
-                                    Some(val) => Some(val-1),
-                                    None => None,
+                                Some(val) => Some(val-1),
+                                None => None,
                             };
                             vec.push((v,vt,vn));
                         }
@@ -157,12 +157,69 @@ impl ObjData {
         }
         return Ok(data);
     }
+
+    /// Write in wavefront format in file.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::fs::File;
+    /// use std::io::BufWriter;
+    /// use std::io::BufReader;
+    /// use lwobj::ObjData;
+    ///
+    /// let f1 = File::open("cube.obj").unwrap();
+    /// let mut input = BufReader::new(f1);
+    /// let data = ObjData::load(&mut input).ok().unwrap();
+    /// let f2 = File::create("tmp.obj").unwrap();
+    /// let mut output = BufWriter::new(f2);
+    /// assert!(data.write(&mut output).is_ok());
+    /// ```
+    pub fn write<W : io::Write>(&self, output : &mut io::BufWriter<W>) -> Result<(),LoadingError> {
+        // Write vertices
+        for &(x,y,z,w) in &self.vertices {
+            let line : String = format!("v {} {} {} {}\n",x,y,z,w);
+            try!(output.write_all(line.as_bytes()));
+        }
+
+        // Write normals
+        for &(x,y,z) in &self.normals {
+            let line : String = format!("vn {} {} {}\n",x,y,z);
+            try!(output.write_all(line.as_bytes()));
+        }
+
+        // Write texcoords
+        for &(u,v,w) in &self.texcoords {
+            let line : String = format!("vt {} {} {}\n",u,v,w);
+            try!(output.write_all(line.as_bytes()));
+        }
+
+        // Write faces
+        for indexes in &self.faces {
+            try!(output.write_all("f".as_bytes()));
+            for &(v,vt,vn) in indexes {
+                let vt_str = match vt {
+                    Some(val) => (val+1).to_string(),
+                    None => "".to_string(),
+                };
+                let vn_str = match vn {
+                    Some(val) => (val+1).to_string(),
+                    None => "".to_string(),
+                };
+                let arg : String = format!(" {}/{}/{}",v+1,vt_str,vn_str);
+                try!(output.write_all(arg.as_bytes()));
+            }
+            try!(output.write_all("\n".as_bytes()));
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::fs::File;
     use std::io::BufReader;
+    use std::io::BufWriter;
     use obj::*;
 
     #[test]
@@ -203,4 +260,49 @@ mod tests {
         assert_eq!(expected.texcoords,data.texcoords);
         assert_eq!(expected.faces,data.faces);
     }
+
+    #[test]
+    fn write() {
+        let mut expected = ObjData::new();
+        expected.vertices = vec![(1.,-1.,-1.,1.),
+        (1.,-1.,1.,1.),
+        (-1.,-1.,1.,1.),
+        (-1.,-1.,-1.,1.),
+        (1.,1.,-1.,1.),
+        (1.,1.,1.,1.),
+        (-1.,1.,1.,1.),
+        (-1.,1.,-1.,1.)];
+        expected.normals = vec![(0.,-1.,0.),
+        (0.,1.,0.),
+        (1.,0.,0.),
+        (0.,0.,1.),
+        (-1.,0.,0.),
+        (0.,0.,-1.)];
+        expected.faces = vec![ vec![(1,None,Some(0)), (3,None,Some(0)), (0,None,Some(0))],
+        vec![(7,None,Some(1)), (5,None,Some(1)), (4,None,Some(1))],
+        vec![(4,None,Some(2)), (1,None,Some(2)), (0,None,Some(2))],
+        vec![(5,None,Some(3)), (2,None,Some(3)), (1,None,Some(3))],
+        vec![(2,None,Some(4)), (7,None,Some(4)), (3,None,Some(4))],
+        vec![(0,None,Some(5)), (7,None,Some(5)), (4,None,Some(5))],
+        vec![(1,None,Some(0)), (2,None,Some(0)), (3,None,Some(0))],
+        vec![(7,None,Some(1)), (6,None,Some(1)), (5,None,Some(1))],
+        vec![(4,None,Some(2)), (5,None,Some(2)), (1,None,Some(2))],
+        vec![(5,None,Some(3)), (6,None,Some(3)), (2,None,Some(3))],
+        vec![(2,None,Some(4)), (6,None,Some(4)), (7,None,Some(4))],
+        vec![(0,None,Some(5)), (3,None,Some(5)), (7,None,Some(5))],
+        ];
+        {
+            let f2 = File::create("tmp.obj").unwrap();
+            let mut output = BufWriter::new(f2);
+            assert!(expected.write(&mut output).is_ok());
+        }
+        let f1 = File::open("tmp.obj").unwrap();
+        let mut input = BufReader::new(f1);
+        let data = ObjData::load(&mut input).ok().unwrap();
+        assert_eq!(expected.vertices,data.vertices);
+        assert_eq!(expected.normals,data.normals);
+        assert_eq!(expected.texcoords,data.texcoords);
+        assert_eq!(expected.faces,data.faces);
+    }
+
 }
